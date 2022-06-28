3d38877f2fafb3c69c898c18f477fd65f4598fc5
Harshal Fosi <95953384+HarshalFosi@users.noreply.github.com>
2022-06-28 08:23:25 +0530

    added new hero

--- Hero Section/Hero#1.jsx
+++ Hero Section/Hero#1.jsx
@@ -1,9 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import { Navbar } from "../components/Navbar";
-import { Footer } from "../components/Footer";
-import { urlObjectKeys } from "next/dist/shared/lib/utils";
+
 
 export default function Home() {
   return (
